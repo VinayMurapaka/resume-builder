c1bb7462d6de7c95e9b3de412c127c5dfd965e92
VinayMurapaka <161616358+VinayMurapaka@users.noreply.github.com>
2024-07-14 15:48:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// form repeater
+$(document).ready(function(){
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show:function(){
+            $(this).slideDown();
+        },
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generate();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
